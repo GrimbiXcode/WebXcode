839b16bcf80e989417cac7ae151755c71f0d4c1c
David Grimbichler <david@grimbichler.ch>
2015-07-28 08:57:27 +0200

    Create Projekt

---
+++ WebXcode/WebXcode/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebXcode
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /***********************************************************************
+         * Definition of attributes
+         * ********************************************************************/
+        private string mAdress;
+        private string mDatabase;
+        private string mUser;
+        private string mPassword;
+        private string mPasswordEncoded;
+        private string mKey;
+
+        private MySQLClient sqlClient;
+
+        /***********************************************************************
+         * Main Code / System Methodes
+         * ********************************************************************/
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            /* Load Settings */
+            mAdress = Properties.Settings.Default.adress;
+            _tboAdress.Text = mAdress;
+            mDatabase = Properties.Settings.Default.database;
+            _tboDatabase.Text = mDatabase;
+            mUser = Properties.Settings.Default.user;
+            _tboUser.Text = mUser;
+            mPassword = Properties.Settings.Default.password;
+            mKey = Properties.Settings.Default.key;
+            mPasswordEncoded = decodePassword(mPassword, mKey);
+            _pboPassword.Password = mPasswordEncoded;
+
+            sqlClient = new MySQLClient(mAdress, mDatabase, mUser, mPasswordEncoded);
+        }
+
+        private string decodePassword(string password, string key) 
+        {
+            return password;
+        }
+
+        private void renewSqlData()
+        {
+            mAdress = _tboAdress.Text;
+            mDatabase = _tboDatabase.Text;
+            mUser = _tboUser.Text;
+            mPasswordEncoded = _pboPassword.Password;
+        }
+
+        /***********************************************************************
+         * Main Code / Event Methodes
+         * ********************************************************************/
+        private void _btnCheckConnection_Click(object sender, RoutedEventArgs e)
+        {
+            renewSqlData();
+
+            if (sqlClient.ConnectionActive == true)
+            {
+                _tboConnectionState.Text = "Verbunden";
+                    _tboConnectionState.Background = Brushes.LightGreen;
+            }
+            else
+            {
+                sqlClient.setUp(mAdress, mDatabase, mUser, mPasswordEncoded);
+                if (sqlClient.requestConnection() == true)
+                {
+                    _tboConnectionState.Text = "Verbunden";
+                    _tboConnectionState.Background = Brushes.LightGreen;
+                }
+                else
+                {
+                    _tboConnectionState.Text = "Getrennt";
+                    _tboConnectionState.Background = Brushes.Salmon;
+                }
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            /* Save Settings */
+            Properties.Settings.Default.adress = mAdress;
+            Properties.Settings.Default.user = mUser;
+            Properties.Settings.Default.password = mPasswordEncoded;
+            Properties.Settings.Default.key = mKey;
+            Properties.Settings.Default.Save();
+            sqlClient.close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            /* Save Settings *
+            Properties.Settings.Default.adress = mAdress;
+            Properties.Settings.Default.user = mUser;
+            Properties.Settings.Default.password = mPasswordEncoded;
+            Properties.Settings.Default.key = mKey;
+            Properties.Settings.Default.Save();*/
+            sqlClient.close();
+        }
+    }
+}
---
+++ WebXcode/WebXcode/MySQLClient.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+/* http://www.codeproject.com/Tips/423233/How-to-Connect-to-MySQL-Using-Csharp 
+ *Server=myServerAddress;Port=1234;Database=myDataBase;Uid=myUsername;Pwd=myPassword;
+ *
+ * You don't need to specify the port if you are using 3306 since that is the default.
+ **/
+
+namespace WebXcode
+{
+    class MySQLClient
+    {
+        /***********************************************************************
+         * Definition of attributes
+         * ********************************************************************/
+        private MySqlConnection myConnection;
+
+        private string mSqlConnectionString;
+        private bool mConnectionActive = false;
+
+        /***********************************************************************
+         * Main Code / System Methodes
+         * ********************************************************************/
+        public MySQLClient(string adress, string database, string user, string password) 
+        {
+            mSqlConnectionString = "Server="+adress+";" +
+                                    "Port=3306;" +
+                                    "Database="+database+";" +
+                                    "Uid="+user+";" +
+                                    "Pwd="+password+";";
+        }
+
+        public void setUp(string adress, string database, string user, string password)
+        {
+            mSqlConnectionString = "Server=" + adress + ";" +
+                                    "Port=3306;" +
+                                    "Database=" + database + ";" +
+                                    "Uid=" + user + ";" +
+                                    "Pwd=" + password + ";";
+        }
+
+        public bool requestConnection() 
+        {
+            myConnection = new MySqlConnection(mSqlConnectionString);
+            try
+            {
+                myConnection.Open();
+                return true;
+                mConnectionActive = true;
+            }
+            catch(Exception e) 
+            {
+                System.Windows.MessageBox.Show(e.ToString(), "SQL-Error", System.Windows.MessageBoxButton.OK);
+                try
+                {
+                    myConnection.Close();
+                }
+                catch (Exception ee)
+                {
+                    System.Windows.MessageBox.Show(ee.ToString(), "SQL-Error", System.Windows.MessageBoxButton.OK);
+                }
+                mConnectionActive = false;
+                return false;
+            }
+        }
+
+        public void close()
+        {
+            try
+            {
+                myConnection.Close();
+            }
+            catch (Exception e) 
+            {
+ 
+            }
+        }
+
+        /***********************************************************************
+         * Main Code / Properties 
+         * ********************************************************************/
+
+        public bool ConnectionActive 
+        {
+            get { return mConnectionActive; }
+            set { }
+        }
+    }
+}
